af452a12030a0b5ab45718dd23944b74a00ab4b5
Ivanitsky Alexey <lexa3556@gmail.com>
2020-09-16 23:05:50 +0300

    Refactoring

--- src/main/java/com/runbatfile/demo/AppStartupRunner.java
+++ src/main/java/com/runbatfile/demo/AppStartupRunner.java
@@ -1,18 +1,22 @@
 package com.runbatfile.demo;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AppStartupRunner implements ApplicationRunner {
+public class AppStartupRunner implements CommandLineRunner {
 
     @Value("${path.to.bat.file}")
     private String pathToBatFile;
 
     @Override
-    public void run(ApplicationArguments args) throws Exception {
-        Runtime.getRuntime().exec(pathToBatFile);
+    public void run(String... args) throws Exception {
+        System.out.println(" --- Start bat file");
+        Runtime.getRuntime().exec("cmd /c start \"\" " + pathToBatFile);
+        System.out.println(" --- End bat file");
+
+        SpringApplication.exit(RunbatfileApplication.context, () -> 0);
     }
 }
--- src/main/java/com/runbatfile/demo/RunbatfileApplication.java
+++ src/main/java/com/runbatfile/demo/RunbatfileApplication.java
@@ -2,12 +2,22 @@ package com.runbatfile.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class RunbatfileApplication {
+public class RunbatfileApplication extends SpringBootServletInitializer {
+
+    protected static ConfigurableApplicationContext context;
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RunbatfileApplication.class);
+    }
 
     public static void main(String[] args) {
-        SpringApplication.run(RunbatfileApplication.class, args);
+        context = SpringApplication.run(RunbatfileApplication.class, args);
     }
 
 }
